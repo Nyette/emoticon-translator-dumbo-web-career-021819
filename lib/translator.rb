b29cc4931f9aef18af64d6c030302fd9ca4a2455
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-18 04:33:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,6 +27,7 @@ def get_english_meaning(file_path, japanese_emoticon)
   emoticons_hash = load_library(file_path)
   if emoticons_hash["get_meaning"].has_key?(japanese_emoticon)
     english_meaning = emoticons_hash[get_meaning][japanese_emoticon]
+    english_meaning
   else
     "Sorry, that emoticon was not found"
   end
